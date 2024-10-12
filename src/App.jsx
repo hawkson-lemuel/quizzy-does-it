ea796ee36262ff81e6a44a16f0c1fe864c2ecd4d
Lemuel Hawkson <digiwebguy@gmail.com>
2024-10-12 10:27:44 +0000

    removed routes that haven't yet been created form App.jsx. updated .gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import QuizCategoriesPage from './components/QuizCategoriesPage'
-import QuizSettingsPage from './components/QuizSettingsPage';
-import QuizHistoryPage from './components/QuizHistoryPage';
-import QuestionPage from './components/QuestionPage';
-import ResultsPage from './components/ResultsPage';
-import {QUESTION, QUIZ_CATEGORIES, QUIZ_HISTORY, QUIZ_RESULTS, QUIZ_SETTINGS} from './utils/routes'
+import {QUIZ_CATEGORIES} from './utils/routes'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,10 +12,6 @@ function App() {
       <Router>
         <Routes>
           <Route path={QUIZ_CATEGORIES} element={<QuizCategoriesPage />} />
-          <Route path={QUIZ_SETTINGS} element={<QuizSettingsPage />} />
-          <Route path={QUIZ_HISTORY} element={<QuizHistoryPage />} />
-          <Route path={QUESTION} element={<QuestionPage />} />
-          <Route path={QUIZ_RESULTS} element={<ResultsPage />} />
         </Routes>
       </Router>
     </>
