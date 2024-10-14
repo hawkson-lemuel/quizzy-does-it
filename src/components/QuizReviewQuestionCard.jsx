bc1feb8409551c281a4060d0e6fc107ec19ec926
Lemuel Hawkson <digiwebguy@gmail.com>
2024-10-14 17:06:39 +0000

    dangerouslysetinnerhtml to render quiz review

--- src/components/QuizReviewQuestionCard.jsx
+++ src/components/QuizReviewQuestionCard.jsx
@@ -5,10 +5,10 @@ export default function QuizReviewQuestionCard({ question, answer, userAnswer })
     };
     return (
         <div className="quiz-review-question-card" style={{marginBottom:'30px'}}>
-            <div className="question">{question}</div>
+            <div className="question" dangerouslySetInnerHTML={{ __html:question}}></div>
             <div style={{paddingLeft:'10px', borderLeft:`4px solid ${answer === userAnswer ? '#57CD75': '#DC5C5C'}`, marginTop:'5px'}}>
-                <p className="quiz-review-system-answer">Answer: {answer}</p>
-                <p className={getAnswerClassName()}>Your Answer: {userAnswer ? userAnswer: "N/A"}</p>
+                <p className="quiz-review-system-answer" dangerouslySetInnerHTML={{ __html:"Answer: " + answer}} ></p>
+                <p className={getAnswerClassName()} dangerouslySetInnerHTML={{ __html: "Your Answer: " + (userAnswer ? userAnswer : "N/A") }}></p>
             </div>
                 
         </div>
