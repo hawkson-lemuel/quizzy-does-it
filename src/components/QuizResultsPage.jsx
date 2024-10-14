f56f5d468e14e6595bb72536512a12ca3a167cd5
Lemuel Hawkson <digiwebguy@gmail.com>
2024-10-14 17:07:13 +0000

    error states for quiz review and quiz results when there's no data to display

--- src/components/QuizResultsPage.jsx
+++ src/components/QuizResultsPage.jsx
@@ -10,7 +10,7 @@ import { useNavigate, Link } from 'react-router-dom';
 
 export default function QuizResultsPage() {
 
-    const { getCorrectAnswerCount, numberOfQuestions, resetQuiz} = useQuizSettingsStore();
+    const { activeQuestions:questions, getCorrectAnswerCount, numberOfQuestions, resetQuiz} = useQuizSettingsStore();
     const navigate = useNavigate();
 
     const handleRetakeQuiz = () => {
@@ -37,21 +37,35 @@ export default function QuizResultsPage() {
         <>
             <Header button={{text:"Back Home", route:QUIZ_CATEGORIES}}/>
             <div className="content-wrapper">
-                    <h2 style={{fontSize:'30px', color:'#FE88A6'}}>{renderQuizPerformanceHeadline()}</h2>
-                    <span style={{marginBottom:'50px', display:'flex',flexDirection:'column',backgroundImage:`url(${confettiImg})`, backgroundSize:'contain', alignItems:'center', backgroundPosition:'center', backgroundRepeat:'no-repeat', width:'100%'}}>
-                        <img src={trophyImg} style={{width:'140px'}} />
-                        <span style={{fontWeight:"bold", }}>
-                            <span style={{color:'#FE88A6', fontSize:'32px'}}>{getCorrectAnswerCount()}</span><span style={{color:'#C6B6B9', fontSize:'20px', position:'relative', top:'-3px'}}>/{numberOfQuestions}</span>
-                        </span>
+                {
+                    questions.length === 0 ? (
+                        <>
+                            <div className="no-history">No quiz results available</div>
+                            <Link to={QUIZ_CATEGORIES}>
+                                <button className="main-header-button">Back to Home</button>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <h2 style={{fontSize:'30px', color:'#FE88A6'}}>{renderQuizPerformanceHeadline()}</h2>
+                            <span style={{marginBottom:'50px', display:'flex',flexDirection:'column',backgroundImage:`url(${confettiImg})`, backgroundSize:'contain', alignItems:'center', backgroundPosition:'center', backgroundRepeat:'no-repeat', width:'100%'}}>
+                                <img src={trophyImg} style={{width:'140px'}} />
+                                <span style={{fontWeight:"bold", }}>
+                                    <span style={{color:'#FE88A6', fontSize:'32px'}}>{getCorrectAnswerCount()}</span><span style={{color:'#C6B6B9', fontSize:'20px', position:'relative', top:'-3px'}}>/{numberOfQuestions}</span>
+                                </span>
+        
+                            </span>
+                            <div>
+                                <Link to={QUIZ_REVIEW}>
+                                    <button className="regular-button" style={{width:'155px', marginRight:'10px'}}>Check Answers</button>
+                                </Link>
+                                <button className="regular-button" style={{backgroundColor:'#57CD75', marginRight:'10px'}} onClick={handleRetakeQuiz}>Retake Quiz!</button>
+                            </div>
+                            <Link to={QUIZ_CATEGORIES} style={{textDecoration:'none', color:'#FE88A6', fontSize:'16px', marginTop:'20px'}}>Choose Another Topic</Link>
+                        </>
 
-                    </span>
-                    <div>
-                        <Link to={QUIZ_REVIEW}>
-                            <button className="regular-button" style={{width:'155px', marginRight:'10px'}}>Check Answers</button>
-                        </Link>
-                        <button className="regular-button" style={{backgroundColor:'#57CD75', marginRight:'10px'}} onClick={handleRetakeQuiz}>Retake Quiz!</button>
-                    </div>
-                    <Link to={QUIZ_CATEGORIES} style={{textDecoration:'none', color:'#FE88A6', fontSize:'16px', marginTop:'20px'}}>Choose Another Topic</Link>
+                    )
+                }
 
             </div>
         </>
--- src/components/QuizReviewPage.jsx
+++ src/components/QuizReviewPage.jsx
@@ -1,32 +1,52 @@
-import { QUIZ_RESULTS } from "../utils/routes";
+import { QUIZ_CATEGORIES, QUIZ_RESULTS } from "../utils/routes";
 import { useQuizSettingsStore } from '../store/quizSettingsStore';
 import Header from "./Header";
 import QuizReviewQuestionCard from "./QuizReviewQuestionCard";
 import "../quizReviewPage.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function QuizReviewPage() {
     const {activeQuestions: questions, answers, category, getCorrectAnswerCount, numberOfQuestions} = useQuizSettingsStore();
+
+    useEffect(() => {
+        console.log(questions);
+    }, []);
     return (
         <>
             <Header button={{text:"Back To Results", route:QUIZ_RESULTS}} />
             <div className="content-wrapper">
             <h2 className="page-title" style={{marginBottom:'0px'}}>Quiz Review</h2>
 
-            <h2 style={{marginBottom:'40px'}}>{category.name} ({getCorrectAnswerCount()}/{numberOfQuestions})</h2>
-            <div>
-                {
-                    questions.map((question, index) => {
-                        return (
-                            <QuizReviewQuestionCard 
-                                key={index}
-                                question={question.question}
-                                answer={question.correct_answer}
-                                userAnswer={answers[index]}
-                            />
-                        )
-                    })
-                }
-            </div>
+            {
+                questions.length === 0 ? (
+                    <>
+                        <div className="no-history">No quiz results available</div>
+                        <Link to={QUIZ_CATEGORIES}>
+                            <button className="main-header-button">Back to Home</button>
+                        </Link>
+                    </>
+                ) :(
+                    <>
+                        <h2 style={{marginBottom:'40px'}}>{category.name} ({getCorrectAnswerCount()}/{numberOfQuestions})</h2>
+                        <div>
+                            {
+                                questions.map((question, index) => {
+                                    return (
+                                        <QuizReviewQuestionCard 
+                                            key={index}
+                                            question={question.question}
+                                            answer={question.correct_answer}
+                                            userAnswer={answers[index]}
+                                        />
+                                    )
+                                })
+                            }
+                        </div>
+                    </>
+                )
+            }
+
             </div>
         </>
     )
